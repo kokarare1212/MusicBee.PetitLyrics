066d2496b214b73961346bb4c8eddeb0feef2395
kokarare1212 <kokarare1212@gmail.com>
2020-09-25 21:12:27 +0900

    commit

--- MusicBee.PetitLyrics/Plugin.cs
+++ MusicBee.PetitLyrics/Plugin.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Text;
-using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
 namespace MusicBeePlugin
 {
@@ -11,8 +11,7 @@ namespace MusicBeePlugin
         private PluginInfo about = new PluginInfo();
         private const string Provider = "プチリリ";
         private static readonly string[] Providers = { Provider };
-        private const string petitClientAppId = "p1110417";
-
+        private const string petitlyricsClientAppId = "p1110417";
         public PluginInfo Initialise(IntPtr apiInterfacePtr)
         {
             mbApiInterface = new MusicBeeApiInterface();
@@ -24,15 +23,14 @@ namespace MusicBeePlugin
             about.TargetApplication = "";
             about.Type = PluginType.LyricsRetrieval;
             about.VersionMajor = 1;
-            about.VersionMinor = 0;
+            about.VersionMinor = 1;
             about.Revision = 0;
-            about.MinInterfaceVersion = MinInterfaceVersion;
-            about.MinApiRevision = MinApiRevision;
+            about.MinInterfaceVersion = 1;
+            about.MinApiRevision = 1;
             about.ReceiveNotifications = (ReceiveNotificationFlags.PlayerEvents | ReceiveNotificationFlags.TagEvents);
             about.ConfigurationPanelHeight = 0;
             return about;
         }
-
         public bool Configure(IntPtr panelHandle)
         {
             return false;
@@ -69,18 +67,21 @@ namespace MusicBeePlugin
         }
         private string GetLyricsByMetadata(string artist, string trackTitle, string album)
         {
+            artist = Uri.EscapeDataString(artist);
+            trackTitle = Uri.EscapeDataString(trackTitle);
+            album = Uri.EscapeDataString(album);
             var client = new WebClient();
             client.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
-            byte[] postData = System.Text.Encoding.UTF8.GetBytes($"clientAppId={petitClientAppId}&lyricsType=1&terminalType=10&key_artist={artist}&key_title={trackTitle}&key_album={album}");
-            var resp = client.UploadData("https://p1.petitlyrics.com/api/GetPetitLyricsData.php", postData);
+            byte[] postData = System.Text.Encoding.UTF8.GetBytes($"clientAppId={petitlyricsClientAppId}&lyricsType=1&terminalType=10&key_artist={artist}&key_title={trackTitle}&key_album={album}");
+            var resp = client.UploadData("http://p1.petitlyrics.com/api/GetPetitLyricsData.php", postData);
             var musicMetadataStr = Encoding.UTF8.GetString(resp);
-            var lyrics = Encoding.UTF8.GetString(
-                Convert.FromBase64String(
-                    Regex.Match(musicMetadataStr, "<lyricsData>(?<lyrics>.*)</lyricsData>")
-                        .Groups["lyrics"].Value
-                )
-            );
-            return lyrics;
+            var musicMetadataX = XDocument.Parse(musicMetadataStr);
+            var lyricsElement = musicMetadataX.Element("lyricsData");
+            if (lyricsElement == null)
+                return "";
+            var encodedLyrics = lyricsElement.Value;
+            var decodedLyrics = Encoding.UTF8.GetString(Convert.FromBase64String(encodedLyrics));
+            return decodedLyrics;
         }
     }
 }
--- MusicBee.PetitLyrics/Properties/AssemblyInfo.cs
+++ MusicBee.PetitLyrics/Properties/AssemblyInfo.cs
@@ -1,12 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("mb_PetitLyrics")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MusicBee PetitLyrics Plugin")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("mb_PetitLyrics")]
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
